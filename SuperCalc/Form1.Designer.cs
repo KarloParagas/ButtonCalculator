7f78e1a68472985bb401d3da8b5189f965fe4ac7
KarloParagas <karloparagas@gmail.com>
2019-10-27 00:17:52 -0700

    Numbers and operator buttons are now displayed when clicked. No calculations are currently performed, back and equals button aren't currently functional.

--- SuperCalc/Form1.Designer.cs
+++ SuperCalc/Form1.Designer.cs
@@ -82,6 +82,7 @@
             this.clearButton.TabIndex = 2;
             this.clearButton.Text = "Clear";
             this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // sevenButton
             // 
--- SuperCalc/Form1.cs
+++ SuperCalc/Form1.cs
@@ -15,6 +15,35 @@ namespace SuperCalc
         public Form1()
         {
             InitializeComponent();
+            zeroButton.Click += new System.EventHandler(ClickedButton);
+            oneButton.Click += new System.EventHandler(ClickedButton);
+            twoButton.Click += new System.EventHandler(ClickedButton);
+            threeButton.Click += new System.EventHandler(ClickedButton);
+            fourButton.Click += new System.EventHandler(ClickedButton);
+            fiveButton.Click += new System.EventHandler(ClickedButton);
+            sixButton.Click += new System.EventHandler(ClickedButton);
+            sevenButton.Click += new System.EventHandler(ClickedButton);
+            eightButton.Click += new System.EventHandler(ClickedButton);
+            nineButton.Click += new System.EventHandler(ClickedButton);
+            divideButton.Click += new System.EventHandler(ClickedButton);
+            multiplyButton.Click += new System.EventHandler(ClickedButton);
+            minusButton.Click += new System.EventHandler(ClickedButton);
+            plusButton.Click += new System.EventHandler(ClickedButton);
+            sqrtButton.Click += new System.EventHandler(ClickedButton);
+            fractionButton.Click += new System.EventHandler(ClickedButton);
+            decimalButton.Click += new System.EventHandler(ClickedButton);
+            posNegButton.Click += new System.EventHandler(ClickedButton);
         }
+
+        public void ClickedButton(object sender, EventArgs e) 
+        {
+            displayBox.Text += (sender as Button).Text;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            displayBox.Text = "";
+        }
+
     }
 }
